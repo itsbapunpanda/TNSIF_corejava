79cb51c6c3ebe70485c3a814c4001d2eea8cb579
itsbapunpanda <149247070+itsbapunpanda@users.noreply.github.com>
2024-12-13 08:59:21 +0530

    Add files via upload

---
+++ TNSIFjava/com/tnsif/scanner/inheritance/singlelevel/SLInheritanceDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.scanner.inheritance.singlelevel;
+
+public class SLInheritanceDemo {
+    public static void main(String[] args) {
+		Student s1= new Student("Gayatri",12344567,"Bangalore",6364520807L,123,"AMC");
+		System.out.println(s1);
+
+	}
+
+}
---
+++ TNSIFjava/com/tnsif/scanner/scannerdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.scanner;
+
+import java.util.Scanner;
+
+public class scannerdemo {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter  your name:");
+		String n= sc.next();
+		System.out.println("Hello p" + n);
+	}
+}    
